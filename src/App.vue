394a33c79559616c97a117345a078a204f2eb9ca
PabloUcm <pmorient@ucm.es>
2021-07-29 01:15:22 +0200

    Testing with Vue and VueTransitions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <TestComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import TestComponent from "./components/TestComponent.vue";
 
 @Component({
   components: {
     HelloWorld,
+    TestComponent,
   },
 })
 export default class App extends Vue {}
@@ -25,5 +28,9 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+html {
+  background: #1b1b1b;
 }
 </style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="test-main">
+      <button id="test-button" @click='show = !show'>Show</button>
+      <transition name="fade">
+        <p id="title" v-if="show"> {{title}} </p>
+      </transition>
+      <transition name="fade">
+        <button id="contact-button" v-if="show">Contact</button>
+      </transition>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TestComponent extends Vue {
+  title = "Hi, I am the Globbo Glabala.";
+  show = false;
+
+  mounted(){
+      this.show = true;
+  }
+}
+</script>
+
+<style scoped>
+
+ #contact-button{
+    border-radius: 30px;
+    color: white;
+    padding: 10px 20px;
+    border: 2px solid #fe0000;
+    background: #1b1b1b;
+    font-family:'Consolas';
+    font-size: 15px;
+    /* transition: all 0.7s; */
+}
+
+/* #contact-button:hover{
+    background-color: #fe0000;
+    font-size:17px;
+} */
+
+#test-button {
+    display:none;
+} 
+
+#title{
+    font-family:'Consolas';
+    font-size: 40px;
+    color: white;
+    margin-top:5%;
+}
+
+
+/*TRANSITIONS*/
+.fade-enter{
+    opacity: 0;
+}
+
+.fade-enter-to{
+    opacity: 1;
+    transition: opacity 2s;
+}
+
+.fade-leave-to{
+    opacity: 0;
+    transition: opacity 2s;
+}
+</style>
